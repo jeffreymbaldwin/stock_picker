3ab9eec003b17e7c077fc69134152d3c8ccc71c5
Jeff <jmartinbaldwin@gmail.com>
2025-11-27 14:27:32 +0900

    add inner loop start and the price to sub into the method.

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,7 +2,13 @@ def stock_picker(prices)
   best_profit = 0
   best_days = [0,0]
   for i in 0...prices.length
+    for j in (i+1)...prices.length
+
+      
+    end
     
   end
   
 end
+
+prices = [17,3,6,9,12,8,6,1,10]
