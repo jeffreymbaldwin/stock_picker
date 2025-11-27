73b1e0324d4152adab22b1784ab2c5d11acda2da
Jeff <jmartinbaldwin@gmail.com>
2025-11-27 15:14:08 +0900

    add the inner loop and the logic that keeps track of `best_profit` and `best_days`

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,23 @@
 def stock_picker(prices)
+  p prices
   best_profit = 0
   best_days = [0,0]
   for i in 0...prices.length
     for j in (i+1)...prices.length
-
+      profit = prices[j] - prices[i]
+      if profit > best_profit
+        best_profit = profit
+        best_days = [i,j]
+        
+      end
       
     end
     
   end
+  best_days
   
 end
 
 prices = [17,3,6,9,12,8,6,1,10]
+
+p stock_picker(prices)
